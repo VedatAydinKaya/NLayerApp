779c0582b0afa7ffd9a682190195e5aca7772b9e
Vedat <Vedat@DESKTOP-F7B40NP>
2022-07-12 20:00:53 +0300

    core entity

---
+++ Core/ProductFeature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class ProductFeature
+    {
+        public ProductFeature(string color)
+        {
+            Color=color ?? throw new ArgumentNullException(nameof(Color)); 
+        }
+
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int ProductId { get; set; } // ForeignKey 1-1 one to one
+
+        public Product? Product { get; set; }
+    }
+}
