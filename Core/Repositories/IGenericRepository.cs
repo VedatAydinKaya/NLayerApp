3d4da427a1211288aff3059ba3c7461b6326495d
Vedat <Vedat@DESKTOP-F7B40NP>
2022-07-13 11:30:54 +0300

    IGenericRepository added

---
+++ Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        // productRepository.GetAll(x=>x.Id>5).OrderBy.ToListAsync();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+
+        // productRepository.Where(x=>x.Id>5).OrderBy.ToListAsync(); *** 
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+ 
+
+        
+
+    }
+}
