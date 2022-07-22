9c7a131ce5bbe5e552544ecd5867a45ff95ed883
Vedat <Vedat@DESKTOP-F7B40NP>
2022-07-22 09:15:01 +0300

    entity configurations

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        { 
+           
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }  
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            //modelBuilder.ApplyConfiguration(new ProductFeatureConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());  // configures all type  named interface(IEntityTypeConfiguration) by doing reflection
+
+
+            base.OnModelCreating(modelBuilder); 
+        }
+
+
+    }
+}
---
+++ Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            // 1-1
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(X => X.ProductId);
+
+        }
+    }
+}
