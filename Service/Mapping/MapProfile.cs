4b4db63a9bbe1fe27d758dd813943f385e6d3424
Vedat <Vedat@DESKTOP-F7B40NP>
2022-08-07 23:16:40 +0300

    custom response dto && mapping

---
+++ Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Dtos;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product,ProductDto>().ReverseMap();       // (entity >>=> dtos)
+            CreateMap<Category,CategoryDto>().ReverseMap();   
+            CreateMap<ProductFeature,ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdate, Product>();
+        }  
+    }
+}
