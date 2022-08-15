23b94dd5aabb7ce19f57c30f96cdd890dc6b6e96
Vedat <Vedat@DESKTOP-F7B40NP>
2022-08-15 23:20:20 +0300

    custom response dto changed with comments

--- Core/Dtos/CustomResponseDto.cs
+++ Core/Dtos/CustomResponseDto.cs
@@ -9,29 +9,46 @@ namespace Core.Dtos
 {
     public class CustomResponseDto<T>
     {
+        private int yas;
+
+        //public CustomResponseDto( int yas)
+        //{
+        //    _yas = yas;
+        //}
+
         public T Data { get; set; }
+
+        public List<string>? Errors { get; set; }
 
         [JsonIgnore]
         public int StatusCode { get; set; }
-        public List<string> Errors { get; set; }
 
-        public static CustomResponseDto<T> Success(int statusCode, T data) // static factory design pattern ?
+
+        //  now let's create  static methods that returns(responses)  to client that        
+        //  endpoints which has been already requested from clients
+
+
+        /*   STATIC FACTORY METHODS  */ 
+
+        public static CustomResponseDto<T> Success(T data, int statusCode)  // when endpoints responds a requests successfully
+                                                                            //       that will be returbed dtos as an static methods
+
         {
-           return new CustomResponseDto<T> { StatusCode=statusCode,Data=data,Errors=null};
-        }
-        public static CustomResponseDto<T> Success(int statusCode)
-        {
-            return new CustomResponseDto<T> { StatusCode = statusCode};
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode, Errors = null };
         }
 
-        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors) 
+        public static CustomResponseDto<T> Success(int statusCode)   // updated case that doesnt need T Data
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = null };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors) // when request from clients failed case
         {
             return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
         }
-
-        public static CustomResponseDto<T> Fail(int statusCode, string errors)
+        public static CustomResponseDto<T> Fail(int statusCode, string error) 
         {
-            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { errors}  };
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors =new List<string> { error} };
         }
     }
 }
